b9658518d8d7d23c077e2407bb40540bcd286fed
Przemek Socha <przemyslaw.socha@vimn.com>
2019-01-17 22:27:49 +0100

    multi-led version

--- server/blinky.py
+++ server/blinky.py
@@ -2,44 +2,54 @@
 
 from flask import Flask
 from flask import render_template
-from gpiozero import LED, PWMLED
+from gpiozero import PWMLED
 
 app = Flask(__name__)
 
+PINS_WITH_LEDS = [24, 25]
 
+leds = {pin: PWMLED(pin) for pin in PINS_WITH_LEDS}
 
-led = PWMLED(25)
 
 @app.route('/')
 def index():
-  return render_template('index.html')  
-
-@app.route('/on')
-def on():
-  led.on()
-  return render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/off')
-def off():
-  led.off()
-  return render_template('index.html')
+def led(pin):
+    return leds[int(pin)]
 
-@app.route('/blink')
-def blink():
-  led.blink()
-  return render_template('index.html')
 
-@app.route('/pulse')
-def pulse():
-  led.pulse()
-  return render_template('index.html')
+@app.route('/<pin>/on')
+def on(pin):
+    led(pin).on()
+    return render_template('index.html', leds=PINS_WITH_LEDS)
 
-@app.route('/pwm/<val>')
-def pwm(val):
-  led.value=float(val)
-  return render_template('index.html')
+
+@app.route('/<pin>/off')
+def off(pin):
+    led(pin).off()
+    return render_template('index.html', leds=PINS_WITH_LEDS)
+
+
+@app.route('/<pin>/blink')
+def blink(pin):
+    led(pin).blink()
+    return render_template('index.html', leds=PINS_WITH_LEDS)
+
+
+@app.route('/<pin>/pulse')
+def pulse(pin):
+    led(pin).pulse()
+    return render_template('index.html', leds=PINS_WITH_LEDS)
+
+
+@app.route('/<pin>/pwm/<val>')
+def pwm(pin, val):
+    led(pin).value = float(val)
+    return render_template('index.html', leds=PINS_WITH_LEDS)
 
 
 if __name__ == "__main__":
+    led(24).on()
     app.run(host='0.0.0.0', port=80)
