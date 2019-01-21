d2c3fdc3e78b752699d00cdd6db5402db84526f8
Przemek Socha <przemyslaw.socha@vimn.com>
2019-01-22 00:34:28 +0100

    change to bootstrap3, add slider

--- client/main/components/LedBox.js
+++ client/main/components/LedBox.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactBootstrapSlider from 'react-bootstrap-slider';
 
 const SettingButton = ({className, children, onClick, active}) => <button
     style={{marginRight: 6}}
@@ -12,16 +13,25 @@ const SettingButton = ({className, children, onClick, active}) => <button
 const LedBox = ({led}) => {
     return <div className={"col-sm-4"}>
 
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{led.name()}</h5>
-                <SettingButton active={led.setting() === "on"} className={"btn-primary"} onClick={led.on}>On</SettingButton>
-                <SettingButton active={led.setting() === "blink"} className={"btn-secondary"} onClick={led.blink}>Blink</SettingButton>
-                <SettingButton active={led.setting() === "pulse"} className={"btn-secondary"} onClick={led.pulse}>Pulse</SettingButton>
-                <SettingButton active={led.setting() === "off"} className={"btn-danger"} onClick={led.off}>Off</SettingButton>
+        <div className="panel panel-default">
+            <div className="panel-heading">
+                <h3 className="panel-title">{led.name()}</h3>
+            </div>
+            <div className="panel-body">
+                <ReactBootstrapSlider
+                    value={50}
+                    step={1}
+                    max={100}
+                    min={0}
+                />
+                <div>
+                    <SettingButton active={led.setting() === "on"} className={"btn-primary"} onClick={led.on}>On</SettingButton>
+                    <SettingButton active={led.setting() === "blink"} className={"btn-info"} onClick={led.blink}>Blink</SettingButton>
+                    <SettingButton active={led.setting() === "pulse"} className={"btn-info"} onClick={led.pulse}>Pulse</SettingButton>
+                    <SettingButton active={led.setting() === "off"} className={"btn-danger"} onClick={led.off}>Off</SettingButton>
+                </div>
             </div>
         </div>
-
     </div>
 };
 
