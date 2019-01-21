4cdcb9411a0ccacdff8ca8fbce2e2a513529b031
Przemek Socha <przemyslaw.socha@vimn.com>
2019-01-21 23:21:07 +0100

    refactor led_api out

--- server/blinky.py
+++ server/blinky.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 from flask import Flask, render_template, jsonify
+from led_api import led_api
 from time import time
 
 from machine import Machine
 
 app = Flask(__name__)
-
-API_METHODS = ['GET', 'PUT']
+app.register_blueprint(led_api)
 
 
 def render_state():
@@ -18,7 +18,7 @@ def render_state():
 
 @app.route('/')
 def index():
-    return render_template('index.html', leds=[24, 25], cachebuster=time())
+    return render_template('index.html', cachebuster=time())
 
 
 @app.route('/state')
@@ -26,34 +26,5 @@ def state():
     return render_state()
 
 
-@app.route('/led/<pin>/on', methods=API_METHODS)
-def on(pin):
-    Machine.led(pin).on()
-    return render_state()
-
-
-@app.route('/led/<pin>/off', methods=API_METHODS)
-def off(pin):
-    Machine.led(pin).off()
-    return render_state()
-
-
-@app.route('/led/<pin>/blink', methods=API_METHODS)
-def blink(pin):
-    Machine.led(pin).blink()
-    return render_state()
-
-
-@app.route('/led/<pin>/pulse', methods=API_METHODS)
-def pulse(pin):
-    Machine.led(pin).pulse()
-    return render_state()
-
-
-@app.route('/led/<pin>/pwm/<val>', methods=API_METHODS)
-def pwm(pin, val):
-    Machine.led(pin).pwm(val)
-    return render_state()
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80)
---
+++ server/led_api.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, jsonify
+from machine import Machine
+
+led_api = Blueprint('led api', __name__)
+
+API_METHODS = ['GET', 'PUT']
+
+
+def render_state():
+    resp = jsonify(Machine.state())
+    resp.status_code = 200
+    return resp
+
+
+@led_api.route('/led/<pin>/on', methods=API_METHODS)
+def on(pin):
+    Machine.led(pin).on()
+    return render_state()
+
+
+@led_api.route('/led/<pin>/off', methods=API_METHODS)
+def off(pin):
+    Machine.led(pin).off()
+    return render_state()
+
+
+@led_api.route('/led/<pin>/blink', methods=API_METHODS)
+def blink(pin):
+    Machine.led(pin).blink()
+    return render_state()
+
+
+@led_api.route('/led/<pin>/pulse', methods=API_METHODS)
+def pulse(pin):
+    Machine.led(pin).pulse()
+    return render_state()
+
+
+@led_api.route('/led/<pin>/pwm/<val>', methods=API_METHODS)
+def pwm(pin, val):
+    Machine.led(pin).pwm(val)
+    return render_state()
