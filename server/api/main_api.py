74feb3ef76868a13d54930353f71b5168f279500
Przemek Socha <przemyslaw.socha@vimn.com>
2019-04-27 20:49:22 +0200

    refactor api out

---
+++ server/api/main_api.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, jsonify
+from machine import Machine
+from time import time
+
+main_api = Blueprint('main api', __name__)
+
+API_METHODS = ['GET', 'PUT']
+
+
+def render_success():
+    resp = jsonify({'success': True})
+    resp.status_code = 200
+    return resp
+
+
+def render_state():
+    resp = jsonify(Machine.state())
+    resp.status_code = 200
+    return resp
+
+
+@main_api.route('/')
+def index():
+    return render_template('index.html', cachebuster=time())
+
+
+@main_api.route('/state')
+def state():
+    return render_state()
--- server/blinky.py
+++ server/blinky.py
@@ -1,34 +1,16 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, jsonify
-from led_api import led_api
-from fire_api import fire_api
-from neopixels_api import neopixels_api
-from time import time
-
-from machine import Machine
-
+from flask import Flask
+from api.main_api import main_api
+from api.led_api import led_api
+from api.fire_api import fire_api
+from api.neopixels_api import neopixels_api
 
 app = Flask(__name__)
+app.register_blueprint(main_api)
 app.register_blueprint(led_api)
 app.register_blueprint(fire_api)
 app.register_blueprint(neopixels_api)
-
-
-def render_state():
-    resp = jsonify(Machine.state())
-    resp.status_code = 200
-    return resp
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', cachebuster=time())
-
-
-@app.route('/state')
-def state():
-    return render_state()
 
 
 if __name__ == "__main__":
