654e4d82eaebd70c6136b84ad9563d535f4fb7bb
Przemek Socha <przemyslaw.socha@vimn.com>
2019-04-28 13:59:49 +0200

    indexing devices by id

--- server/machine/base_machine.py
+++ server/machine/base_machine.py
@@ -9,22 +9,25 @@ class BaseMachine:
         self._fires = []
         self._neopixels = []
         self._neopixel_strip = None
+        self._devicesById = {}
 
     def add_led(self, pin, name):
         led = LED(pin, name)
         self._leds.append(led)
+        self._devicesById[pin] = led
         return self
 
     def led(self, pin):
-        return next(x for x in self._leds if x.pin == pin)
+        return self.device(pin)
 
     def add_fire(self, pin_r, pin_y, name):
         fire = Fire("fire" + str(len(self._fires)), pin_r, pin_y, name)
         self._fires.append(fire)
+        self._devicesById[fire.id] = fire
         return self
 
     def fire(self, id):
-        return next(x for x in self._fires if x.id == id)
+        return self.device(id)
 
     def set_neopixel_strip(self, num_pixels):
         self._neopixel_strip = NeopixelStrip(num_pixels)
@@ -32,10 +35,14 @@ class BaseMachine:
     def add_neopixel(self, name, pix_from, num_pixels):
         pixels = Neopixel("neopixel" + str(len(self._neopixels)), pix_from, num_pixels, name, self._neopixel_strip)
         self._neopixels.append(pixels)
+        self._devicesById[pixels.id] = pixels
         return self
 
     def neopixel(self, id):
-        return next(x for x in self._neopixels if x.id == id)
+        return self.device(id)
+
+    def device(self, id):
+        return self._devicesById[id]
 
     def state(self):
         return {
