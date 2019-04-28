d77871c4e993ca4f5ea6a7e2811cc5c064eb0417
Przemek Socha <przemyslaw.socha@vimn.com>
2019-04-28 16:11:26 +0200

    refactor neopixel - split into device part and logic part

--- server/machine/neopixels.py
+++ server/machine/neopixels.py
@@ -6,37 +6,15 @@ from machine.rainbow import rainbow_source
 from gpiozero_ps.generators import *
 
 
-class Neopixel(SourceMixin):
-    def __init__(self, id, pix_from, num_pixels, name, neopixel_strip, *args, **kwargs):
-        SourceMixin.__init__(self, *args, **kwargs)
+class Neopixel:
+    def __init__(self, id, pix_from, num_pixels, name, neopixel_strip):
         self.id = id
         self.name = name
-        self.pix_from = pix_from
-        self.num_pixels = num_pixels
-        self._strip = neopixel_strip
-        self._value = [(0, 0, 0) for _ in range(num_pixels)]
-        self._effect_evaluator = Evaluator(num_pixels)
-        self.source = None
         self._setting = 'rgb'
         self._setting_params = {"r": 0.5, "g": 0.5, "b": 0.5, "brightness": 1}
+        self._device = _NeopixelDevice(neopixel_strip, pix_from, num_pixels, self._param_generator("brightness"))
+        self._effect_evaluator = Evaluator(num_pixels)
         self._on = False
-
-    @property
-    def value(self):
-        # list of rgb values
-        return self._value
-
-    @value.setter
-    def value(self, val):
-        if isinstance(val, list):
-            self._value = val
-        else:
-            self._value = [val for i in range(self.num_pixels)]
-        self._apply()
-
-    @property
-    def brightness(self):
-        return self._setting_params['brightness']
 
     @property
     def setting(self):
@@ -51,7 +29,7 @@ class Neopixel(SourceMixin):
 
     def off(self):
         self._on = False
-        self._set_sources([constant_source(0, 0, 0) for _ in range(self.num_pixels)])
+        self._device.set_sources([constant_source(0, 0, 0) for _ in range(self._device.num_pixels)])
 
     def set_param(self, param_name, value):
         self._setting_params[param_name] = value
@@ -76,25 +54,25 @@ class Neopixel(SourceMixin):
             raise Exception("Unknown setting: " + self.setting)
 
     def _fire(self):
-        self._set_sources([fire_source() for _ in range(self.num_pixels)])
+        self._device.set_sources([fire_source() for _ in range(self._device.num_pixels)])
 
     def _rgb(self):
         set_defaults(self._setting_params, {'r': 0.5, 'g': 0.5, 'b': 0.5})
-        self._set_sources([constant_source(
+        self._device.set_sources([constant_source(
             self._setting_params["r"],
             self._setting_params["g"],
             self._setting_params["b"]
-        ) for _ in range(self.num_pixels)])
+        ) for _ in range(self._device.num_pixels)])
 
     def _rainbow(self):
-        self._set_sources([rainbow_source(
+        self._device.set_sources([rainbow_source(
                 speed_generator=self._param_generator('speed'),
-                offset=float(i) * 256 / self.num_pixels
-            ) for i in range(self.num_pixels)])
+                offset=float(i) * 256 / self._device.num_pixels
+            ) for i in range(self._device.num_pixels)])
 
-    def effect(self):
+    def _effect(self):
         self._effect_evaluator.body = self._setting_params["body"]
-        self._set_sources([self._effect_evaluator.get_source() for _ in range(self.num_pixels)])
+        self._device.set_sources([self._effect_evaluator.get_source() for _ in range(self._device.num_pixels)])
 
     def state(self):
         return {
@@ -103,21 +81,8 @@ class Neopixel(SourceMixin):
             'on': self._on,
             'setting': self.setting,
             'params': self._setting_params,
-            'brightness': self.brightness
+            'brightness': self._setting_params['brightness']
         }
-
-    def _set_sources(self, sources):
-        def generator():
-            while True:
-                yield [next(source) for source in sources]
-        self.source = generator()
-        self.value = next(self.source)
-        self._apply()
-
-    def _apply(self):
-        for i in range(self.num_pixels):
-            (r, g, b) = self._value[i]
-            self._strip.set_pixel(self.pix_from + i, (r * self.brightness, g * self.brightness, b * self.brightness))
 
     def _param_generator(self, name):
         while True:
@@ -142,6 +107,7 @@ class Evaluator:
 
     def get_source(self):
         r, g, b = self._eval_body()
+
         def generator():
             while True:
                 yield (next(r), next(g), next(b))
@@ -160,3 +126,43 @@ class Evaluator:
         return eval(self._body)
 
 
+class _NeopixelDevice(SourceMixin):
+    def __init__(self, neopixel_strip, pix_from, num_pixels, brightness_generator):
+        SourceMixin.__init__(self)
+        self._strip = neopixel_strip
+        self.pix_from = pix_from
+        self._num_pixels = num_pixels
+        self._value = [(0, 0, 0) for _ in range(num_pixels)]
+        self._brightness_generator = brightness_generator
+        self.source = None
+
+    @property
+    def value(self):
+        # list of rgb values
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, list):
+            self._value = val
+        else:
+            self._value = [val for i in range(self.num_pixels)]
+        self._apply()
+
+    @property
+    def num_pixels(self):
+        return self._num_pixels
+
+    def set_sources(self, sources):
+        def generator():
+            while True:
+                yield [next(source) for source in sources]
+        self.source = generator()
+        self.value = next(self.source)
+        self._apply()
+
+    def _apply(self):
+        for i in range(self.num_pixels):
+            (r, g, b) = self._value[i]
+            brightness = next(self._brightness_generator)
+            self._strip.set_pixel(self.pix_from + i, (r * brightness, g * brightness, b * brightness))
