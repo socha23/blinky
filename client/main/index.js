662a59654647ff2214c075023cc077260f42f584
Przemek Socha <przemyslaw.socha@vimn.com>
2019-05-05 21:21:52 +0200

    small fixes

--- client/main/index.js
+++ client/main/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Main from "./components/Main";
 import Debug from "./debug/Debug";
 
 const App = () => <Router>
+    <Switch>
         <Route path={"/debug"} component={Debug}/>
         <Route path={"/"} component={Main}/>
+    </Switch>
     </Router>
 ;
 
--- server/machine/components/on_off.py
+++ server/machine/components/on_off.py
@@ -11,19 +11,31 @@ class TurnOnAndOffEffectMixin:
             "fade_out_duration": 0.5,
         })
 
-    def _wrap_source_in_turn_on_effect(self, source):
+    def _wrap_source_in_turn_on_effect(self, source_or_sources):
+        if type(source_or_sources) is tuple or type(source_or_sources) is list:
+            return self._wrap_sources_in_turn_on_effect(source_or_sources)
+        else:
+            return self._wrap_sources_in_turn_on_effect([source_or_sources])[0]
+
+    def _wrap_sources_in_turn_on_effect(self, sources):
         effect = self._setting_params['turn_on_effect']
         if effect == 'fade_in':
-            return fade_in(source, int(200 * self._setting_params['fade_in_duration']))
+            return [fade_in(source, int(200 * self._setting_params['fade_in_duration'])) for source in sources]
         else:
-            return source
+            return sources
 
-    def _wrap_source_in_turn_off_effect(self, source):
+    def _wrap_source_in_turn_off_effect(self, source_or_sources):
+        if type(source_or_sources) is tuple or type(source_or_sources) is list:
+            return self._wrap_sources_in_turn_off_effect(source_or_sources)
+        else:
+            return self._wrap_sources_in_turn_off_effect([source_or_sources])[0]
+
+    def _wrap_sources_in_turn_off_effect(self, sources):
         effect = self._setting_params['turn_off_effect']
         if effect == 'fade_out':
-            return fade_out(source, int(200 * self._setting_params['fade_out_duration']))
+            return [fade_out(source, int(200 * self._setting_params['fade_out_duration'])) for source in sources]
         else:
-            return constant(0)
+            return [constant(0) for _ in sources]
 
 
 def fade_in(source, duration=50):
